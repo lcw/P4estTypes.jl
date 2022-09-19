f08487d49de286ceb24d12863d6a14cf1fb56650
Lucas C Wilcox <lucas@swirlee.com>
2022-09-19 13:23:18 -0700

    Add quadrant user data type to `GhostLayer`

--- src/ghost.jl
+++ src/ghost.jl
@@ -1,15 +1,15 @@
-mutable struct GhostLayer{X,P}
+mutable struct GhostLayer{X,T,P}
     pointer::P
-    function GhostLayer{4}(pointer::Ptr{p4est_ghost_t})
-        ghost = new{4,typeof(pointer)}(pointer)
+    function GhostLayer{4}(pointer::Ptr{p4est_ghost_t}, ::Type{T}) where {T}
+        ghost = new{4,T,typeof(pointer)}(pointer)
         finalizer(ghost) do p
             p4est_ghost_destroy(p.pointer)
             p.pointer = C_NULL
             return
         end
     end
-    function GhostLayer{8}(pointer::Ptr{p8est_ghost_t})
-        ghost = new{8,typeof(pointer)}(pointer)
+    function GhostLayer{8}(pointer::Ptr{p8est_ghost_t}, ::Type{T}) where {T}
+        ghost = new{8,T,typeof(pointer)}(pointer)
         finalizer(ghost) do p
             p8est_ghost_destroy(p.pointer)
             p.pointer = C_NULL
@@ -18,20 +18,20 @@ mutable struct GhostLayer{X,P}
     end
 end
 
-function ghostlayer(forest::Pxest{X}; connection = CONNECT_FULL(Val(X))) where {X}
-    return GhostLayer{X}((pxest_ghost_new(Val(X)))(forest, connection))
+function ghostlayer(forest::Pxest{X,T}; connection = CONNECT_FULL(Val(X))) where {X,T}
+    return GhostLayer{X}((pxest_ghost_new(Val(X)))(forest, connection), T)
 end
 
 function Base.unsafe_convert(
     ::Type{Ptr{p4est_ghost_t}},
-    p::GhostLayer{4,Ptr{p4est_ghost_t}},
-)
+    p::GhostLayer{4,T,Ptr{p4est_ghost_t}},
+) where {T}
     return p.pointer
 end
 
 function Base.unsafe_convert(
     ::Type{Ptr{p8est_ghost_t}},
-    p::GhostLayer{8,Ptr{p8est_ghost_t}},
-)
+    p::GhostLayer{8,T,Ptr{p8est_ghost_t}},
+) where {T}
     return p.pointer
 end
