e4fcd2462e73f65861a655ce281360655ebd0cac
Lucas C Wilcox <lucas@swirlee.com>
2023-04-03 15:00:50 -0700

    Add more pxest tests

--- test/test_pxest.jl
+++ test/test_pxest.jl
@@ -14,10 +14,31 @@ const comm = MPI.COMM_WORLD
 let
     forest = pxest(brick(3, 4); comm)
     @test MPI.Allreduce(sum(length.(forest)), +, comm) == 12
+    @test MPI.Allreduce(lengthoflocalquadrants(forest), +, comm) == 12
+    @test lengthofglobalquadrants(forest) == 12
+    @test connectivity(forest) == brick(3, 4)
+    @test size(forest) == (12,)
+    @test P4estTypes.quadrantstyle(forest) == 4
+    @test P4estTypes.quadrantndims(forest) == 2
 end
 
 let
     forest = pxest(brick(2, 4, 3); comm)
+    @test MPI.Allreduce(sum(length.(forest)), +, comm) == 24
+    @test MPI.Allreduce(lengthoflocalquadrants(forest), +, comm) == 24
+    @test lengthofglobalquadrants(forest) == 24
+    @test connectivity(forest) == brick(2, 4, 3)
+    @test size(forest) == (24,)
+    @test P4estTypes.quadrantstyle(forest) == 8
+    @test P4estTypes.quadrantndims(forest) == 3
+
+    coarsen!(forest)
+    @test MPI.Allreduce(sum(length.(forest)), +, comm) == 24
+    refine!(forest)
+    @test MPI.Allreduce(sum(length.(forest)), +, comm) == 24
+    balance!(forest)
+    @test MPI.Allreduce(sum(length.(forest)), +, comm) == 24
+    partition!(forest)
     @test MPI.Allreduce(sum(length.(forest)), +, comm) == 24
 end
 
@@ -44,8 +65,35 @@ let
     conn = Connectivity{4}(VXYZ, EToV)
     @test isvalid(conn)
 
-    forest = pxest(conn, min_level = 2)
+    struct Foo
+        a::Int32
+        b::Float64
+        c::Float32
+    end
+
+    function foo_init(_, treeid, quadrant)
+        data = Foo(treeid, rand(Float64), rand(Float32))
+        storeuserdata!(quadrant, data)
+        @test loaduserdata(quadrant) == data
+    end
+
+    function foo_check(_, _, quadrant, _, treeid, _)
+        @test level(quadrant) == 2
+        @test coordinates(quadrant) isa NTuple{2}
+        @test loaduserdata(quadrant).a == treeid
+    end
+
+    forest = pxest(conn, min_level = 2, data_type = Foo, init_function = foo_init)
+    iterateforest(forest; volume = foo_check)
+
     @test MPI.Allreduce(sum(length.(forest)), +, comm) == 64
+    @test MPI.Allreduce(lengthoflocalquadrants(forest), +, comm) == 64
+    @test lengthofglobalquadrants(forest) == 64
+    @test connectivity(forest) == conn
+    @test size(forest) == (4,)
+    @test P4estTypes.quadrantstyle(forest) == 4
+    @test P4estTypes.quadrantndims(forest) == 2
+    @test P4estTypes.typeofquadrantuserdata(forest) == Foo
 end
 
 MPI.Finalize()
