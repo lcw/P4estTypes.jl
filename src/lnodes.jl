c7c198d4f0b75e28c8b2b2d927e7d9cf45f81e00
Lucas C Wilcox <lucas@swirlee.com>
2023-05-02 14:54:15 -0700

    Add the associated MPI.Comm to LNodes structure

--- src/lnodes.jl
+++ src/lnodes.jl
@@ -16,16 +16,19 @@ mutable struct LNodes{X,P}
     documentation for these types for more information about the
     underlying p4est structures. """
     pointer::P
-    function LNodes{4}(pointer::Ptr{P4est.LibP4est.p4est_lnodes})
-        nodes = new{4,typeof(pointer)}(pointer)
+    """The MPI Communicator that includes the ranks participating in the
+    lnods."""
+    comm::MPI.Comm
+    function LNodes{4}(pointer::Ptr{P4est.LibP4est.p4est_lnodes}, comm::MPI.Comm)
+        nodes = new{4,typeof(pointer)}(pointer, comm)
         finalizer(nodes) do p
             p4est_lnodes_destroy(p.pointer)
             p.pointer = C_NULL
             return
         end
     end
-    function LNodes{8}(pointer::Ptr{P4est.LibP4est.p8est_lnodes})
-        nodes = new{8,typeof(pointer)}(pointer)
+    function LNodes{8}(pointer::Ptr{P4est.LibP4est.p8est_lnodes}, comm::MPI.Comm)
+        nodes = new{8,typeof(pointer)}(pointer, comm)
         finalizer(nodes) do p
             p8est_lnodes_destroy(p.pointer)
             p.pointer = C_NULL
@@ -52,7 +55,7 @@ function lnodes(forest::Pxest{X}; ghost = nothing, degree = 1) where {X}
     if isnothing(ghost)
         ghost = ghostlayer(forest)
     end
-    return LNodes{X}((pxest_lnodes_new(Val(X)))(forest, ghost, degree))
+    return LNodes{X}((pxest_lnodes_new(Val(X)))(forest, ghost, degree), comm(forest))
 end
 
 function Base.unsafe_convert(::Type{Ptr{p4est_lnodes}}, p::LNodes{4,Ptr{p4est_lnodes}})
