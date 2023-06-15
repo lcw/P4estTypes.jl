95d0cfbfb135bf935e3c280ac4a330b740c1996c
masonamccallum <masonamccallum@gmail.com>
2023-06-15 17:54:21 -0500

    Added docs: Connectivity{X}(vertices,cells)
    2D shell example in docs

--- src/connectivity.jl
+++ src/connectivity.jl
@@ -201,6 +201,43 @@ function Connectivity{8}(name::Symbol, args...)
     return Connectivity{8}(pointer)
 end
 
+
+"""
+    function Connectivity{X}(vertices::AbstractVector, cells::AbstractVector) where {X}
+
+    Example:
+    '''julia
+     julia> vertices = [
+                (0.0, 0.0, 0.0),
+                (0.0, 0.0, 1.0),
+                (0.0, 1.0, 0.0),
+                (0.0, 1.0, 1.0),
+                (1.0, 0.0, 0.0),
+                (1.0, 0.0, 1.0),
+                (1.0, 1.0, 0.0),
+                (1.0, 1.0, 1.0),
+            ]
+     julia> cells = [
+                Int32.((1, 3, 2, 4)),
+                Int32.((4, 3, 8, 7)),
+                Int32.((7, 5, 8, 6)),
+                Int32.((6, 2, 5, 1)),
+                Int32.((1, 3, 5, 7)),
+                Int32.((2, 4, 6, 8)),
+            ]
+     julia> conn = Connectivity{4}(vertices, cells)
+    '''
+       6--------8    cell 1 connects vertices 1, 3, 2, 4
+      /        /|    cell 2 connects vertices 4, 3, 8, 7
+     /        / |    .
+    2--------4  |    .
+    |        |  |    .
+    |  5     |  7    cell 6 connects vertices 2, 4, 6, 8
+    |        | /
+    |        |/
+    1--------3
+ 
+"""
 function Connectivity{X}(vertices::AbstractVector, cells::AbstractVector) where {X}
     if X == 4
         conn = P4estTypes.Connectivity{X}(
